d5ab201f75f76aaa04bd821f21f5a90af721b3be
Trevor Fountain <doches@kornbluth.(none)>
2009-08-29 03:03:01 +0100

    nitial add.

---
+++ Config.py
@@ -0,0 +1,6 @@
+# This file contains some simple, game-wide configuration options, and is designed to be supplanted by something more complex in an actual (e.g. non-development) project.
+
+ScreenSize = (800,600) # Set the initial screen size
+Fullscreen = False # Do fullscreen?
+DoubleBuffer = True # Draw with double-buffering and flipping?
+Framerate = 60 # Set target framerate
